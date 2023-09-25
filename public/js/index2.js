447fce0c21d314df7bd23e5bcefce9cedbef32f1
nodejs-jhow <jonathan.programnodejs@gmail.com>
2023-09-24 22:17:36 -0300

    version 2.0

--- public/js/index2.js
+++ public/js/index2.js
@@ -1,5 +1,12 @@
-const h3 = document.querySelector("h3")
-console.log(h3);
-h3.addEventListener('click',()=>{
-    h3.textContent = "Jeová Gére"
+const p1 = document.querySelector("#information")
+
+p1.addEventListener('click',async()=>{
+    fetch(`https://www.abibliadigital.com.br/api/verses/nvi/gn/1`).then(response => {
+    console.log(response);    
+    return response.json();
+    }).then(data=>{
+        console.log(data);
+        p1.textContent = `Autor do livro foi ${data.book.author}. \n Livro de ${data.book.name}. \n Versículos de ${data.chapter.number}
+         á ${data.chapter.verses}  `
+    })
 })
