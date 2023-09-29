692133357d12d63da35993fcb7b6a293451730a7
nodejs-jhow <jonathan.programnodejs@gmail.com>
2023-09-29 20:53:57 -0300

    finish project

--- public/js/index2.js
+++ public/js/index2.js
@@ -1,61 +1,135 @@
+
 const p1 = document.querySelector("#information")
+
 const div_content = document.querySelector("#content")
+
 const form_select = document.querySelector("#select-book")
+
 const form_captule = document.querySelector("#select-capitule")
+
 const buttom_submit = document.querySelector("#buttom-submit")
+
+const p_invisyble = document.querySelectorAll('.text-temp')
+
+const hidden_form = document.querySelectorAll(".container-form")
+
+const buttom_return = document.querySelector("#btn")
+
 let index = 0
+
 let list_outhors;
+
 let book = [];
 
 //Aqui ele carrega a lista de livros no select
 
 const init = () => {
 
-    fetch(`https://www.abibliadigital.com.br/api/books`).then(response => {  
+    fetch(`https://www.abibliadigital.com.br/api/books`).then(response => {
+
         return response.json();
+
     }).then(data => {
+
         book.push(...data);
-        // //console.log(book);
+
         data.forEach((element) => {
+
             form_select.innerHTML += `<option value="${element.abbrev.pt}">${element.name}</option>`
+
         });
+
     })
-}   
+
+}
+
 init();
+
 //Pega o Livro escolhido e traz os versiculos
+
 form_select.addEventListener("input", () => {
-    book.forEach((element)=>{
+
+    book.forEach((element) => {
+
         //console.log(form_select.value == element.abbrev.pt);
-        if(form_select.value == element.abbrev.pt){
+
+        if (form_select.value == element.abbrev.pt) {
+
             for (let i = 1; i < element.chapters; i++) {
+
                 form_captule.innerHTML += `<option value="${i}">${i}</option>`
+
             }
+
         }
+
         // //console.log("Book elemento=>",element,"versos name", data.book.name);
-    })
 
     })
 
-    form_captule.addEventListener("input", ()=>{
-        //console.log(form_captule.value);
-            fetch(`https://www.abibliadigital.com.br/api/verses/nvi/${form_select.value}/${form_captule.value}`).then(response => {
-                // //console.log(response);    
-                return response.json();
-            }).then(data => {
-                list_outhors = data.book.author.split(",")
-                //console.log(data);
-                 p1.textContent = `Livro ${data.book.name} ` + list_outhors != 0 ? `Autores `:`autor ${data.book.author}`  
-                if(list_outhors != 0){
-                    for (let i = 0; i < list_outhors.length; i++) {
-                        p1.textContent  += `${list_outhors[i]},`;
-    
-                    }
-                    }
-                data.verses.forEach(element => {
-                   index++
-                   div_content.innerHTML += ` ${index} ${element.text}`
-    
-                });
-            })
-    
+})
+
+form_captule.addEventListener("input", () => {
+
+    p_invisyble.forEach(element => {
+
+        element.setAttribute('style', 'display:none')
+
+        console.log(element);
+
     })
+    hidden_form.forEach(element => {
+
+        element.setAttribute('style', 'display:none')
+
+        console.log(element);
+
+    })
+
+    buttom_return.setAttribute("style", "diplay:block")
+
+    //console.log(form_captule.value);
+
+    fetch(`https://www.abibliadigital.com.br/api/verses/nvi/${form_select.value}/${form_captule.value}`).then(response => {
+
+        // //console.log(response);    
+
+        return response.json();
+
+    }).catch(error => console.log(error)).then(data => {
+
+        list_outhors = data.book.author.split(",")
+
+        //console.log(data);
+
+        p1.append(`<h3>Livro ${data.book.name} ` + list_outhors != 0 ? `Autor(a) ` : `Autor(a) ${data.book.author}</h3>`)
+
+        if (list_outhors != 0) {
+
+            for (let i = 0; i < list_outhors.length; i++) {
+
+                p1.append(`${list_outhors[i]}`);
+
+            }
+
+        }
+
+        data.verses.forEach(element => {
+
+            index++
+
+            p1.innerHTML += ` <p>${index} ${element.text}</p>`
+
+
+
+        });
+
+    }).catch(error => console.log(error))
+
+
+
+})
+
+buttom_return.addEventListener("click",()=>{
+    location.reload()
+})
