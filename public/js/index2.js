0a40287165341ca5d1b3c1a30609c3b8c74a6c57
nodejs-jhow <jonathan.programnodejs@gmail.com>
2023-09-28 22:14:45 -0300

    september 28/09

--- public/js/index2.js
+++ public/js/index2.js
@@ -15,7 +15,7 @@ const init = () => {
         return response.json();
     }).then(data => {
         book.push(...data);
-        // console.log(book);
+        // //console.log(book);
         data.forEach((element) => {
             form_select.innerHTML += `<option value="${element.abbrev.pt}">${element.name}</option>`
         });
@@ -24,45 +24,38 @@ const init = () => {
 init();
 //Pega o Livro escolhido e traz os versiculos
 form_select.addEventListener("input", () => {
-        fetch(`https://www.abibliadigital.com.br/api/verses/nvi/${form_select.value}/${'1'}`).then(response => {
-            // console.log(response);    
-            return response.json();
-        }).then(data => {
-            book.forEach((element)=>{
-                console.log(data,"===========>",element);
-                if(data.book.name == element.name){
-                    for (let i = 1; i < element.chapters; i++) {
-                        form_captule.innerHTML += `<option value="${i}">${i}</option>`
-                    }
-                }
-                // console.log("Book elemento=>",element,"versos name", data.book.name);
-            })
-            list_outhors = data.book.author.split(",")
-        })
+    book.forEach((element)=>{
+        //console.log(form_select.value == element.abbrev.pt);
+        if(form_select.value == element.abbrev.pt){
+            for (let i = 1; i < element.chapters; i++) {
+                form_captule.innerHTML += `<option value="${i}">${i}</option>`
+            }
+        }
+        // //console.log("Book elemento=>",element,"versos name", data.book.name);
     })
 
-    const addText =()=>{
-        fetch(`https://www.abibliadigital.com.br/api/verses/nvi/${form_select.value}/2`).then(response => {
-            // console.log(response);    
-            return response.json();
-        }).then(data => {
-            list_outhors = data.book.author.split(",")
-            console.log(data);
-             p1.textContent = `Livro ${data.book.name} ` + list_outhors != 0 ? `Autores `:`autor ${data.book.author}`  
-            if(list_outhors != 0){
-                for (let i = 0; i < list_outhors.length; i++) {
-                    p1.textContent  += `${list_outhors[i]},`;
+    })
 
-                }
-                }
-            data.verses.forEach(element => {
-               index++
-               div_content.innerHTML += ` ${index} ${element.text}`
-
-            });
-        })
-    }
-
-    buttom_submit.addEventListener("click",()=>{
-
+    form_captule.addEventListener("input", ()=>{
+        //console.log(form_captule.value);
+            fetch(`https://www.abibliadigital.com.br/api/verses/nvi/${form_select.value}/${form_captule.value}`).then(response => {
+                // //console.log(response);    
+                return response.json();
+            }).then(data => {
+                list_outhors = data.book.author.split(",")
+                //console.log(data);
+                 p1.textContent = `Livro ${data.book.name} ` + list_outhors != 0 ? `Autores `:`autor ${data.book.author}`  
+                if(list_outhors != 0){
+                    for (let i = 0; i < list_outhors.length; i++) {
+                        p1.textContent  += `${list_outhors[i]},`;
+    
+                    }
+                    }
+                data.verses.forEach(element => {
+                   index++
+                   div_content.innerHTML += ` ${index} ${element.text}`
+    
+                });
+            })
+    
     })
